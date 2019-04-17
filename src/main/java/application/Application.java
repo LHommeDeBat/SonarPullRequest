e328fc190abb2fd6f4a49fdae6ffedaca400deed
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-04-17 18:38:16 +0200

    Edited project

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,9 +1,12 @@
 package application;
 
+import services.Calculator;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Calculator calc = new Calculator();
+		calc.add(2, 2, 2);
 	}
 
 }
---
+++ src/main/java/services/Calculator.java
@@ -0,0 +1,9 @@
+package services;
+
+public class Calculator {
+
+	public Integer add(int a, int b, int c) {
+		return a + b;
+	}
+	
+}
